f4b684857d5339e55d1ab45c608c6dc1daba5419
DevanshRaheja <80957882+DevanshRaheja@users.noreply.github.com>
2024-01-18 20:09:31 +0000

    Understanding the datatypes

---
+++ 01_Basics_of_JS/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict";       // treat all JS code as newer version 
+
+// alert (3 + 3)    // we are using node js not browser, so prompts and alerts will not pop up 
+
+console.log(3 
+    + 
+    3); //code readability should always be high not as shown in example
+
+console.log("Devansh");
+
+let name = "Devansh"    // string
+let age = 21            // number
+let isVerified = true   // boolean
+let state;              //undefined
+
+// number => ranges is 2 * power (53)
+// bigint => for bigger numbers (more range than 'number')
+// string => "" / ''  (prefer using double quotes)
+// boolean => true/false
+// undefined => value is not defined
+// null => standalone value
+// symbol => uniqueness of components
+
+// object
+
+console.log(typeof null);       // object 
+console.log(typeof undefined);  //undefined
