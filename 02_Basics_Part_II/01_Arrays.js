8cfdb1d780b74c4986b8d77fea4d0a82bb8dcd15
DevanshRaheja <80957882+DevanshRaheja@users.noreply.github.com>
2024-01-22 12:51:11 +0000

    Arrays in js

---
+++ 02_Basics_Part_II/01_Arrays.js
@@ -0,0 +1,29 @@
+// Arrays
+
+/*
+-> JavaScript arrays are resizable and can contain a mix of different data types.
+-> JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes.
+-> JavaScript arrays are zero-indexed.
+-> Shallow copy: A copy whose properties share the same references [heap memory or reference]
+-> Deep copy: copy of variable is created without providing the original memory address 
+*/
+
+// Declaration of arrays
+
+const arr = [1,2,3,4,5]
+const myHeroes = ['Iron Man', 'Hulk', 'Loki', 'Wanda']
+
+const myArr = new Array(1,2,3,4)
+
+// console.log(myArr, myHeroes)
+// [ 1, 2, 3, 4 ] [ 'Iron Man', 'Hulk', 'Loki', 'Wanda' ]
+
+// Array Methods
+
+// myArr.push(7) //insert an element in array
+// myArr.push(10)
+// myArr.pop() // remove an element from last in array
+
+,ya
+
+console.log(myArr)
