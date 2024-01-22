6f624a84a589dc10f746a37c8f73184dd33606f3
DevanshRaheja <80957882+DevanshRaheja@users.noreply.github.com>
2024-01-22 10:14:14 +0000

    Important concept related to random

--- 01_Basics_of_JS/06_Numbers_and_Maths.js
+++ 01_Basics_of_JS/06_Numbers_and_Maths.js
@@ -27,11 +27,30 @@ Number
 /************************Maths**************************/
 
 
-console.log(Math);
-console.log(Math.abs(-23));    //23
-console.log(Math.round(4.7));   //5
+// console.log(Math);
+// console.log(Math.abs(-23));    //23
+// console.log(Math.round(4.7));   //5
 
-console.log(Math.ceil(4.7));  // top value => 5
-console.log(Math.floor(4.7)); // below value => 4
-console.log(Math.max(10,2,4,15,20)); //20
-console.log(Math.min(10,2,4,15,20)); //2
+// console.log(Math.ceil(4.7));  // top value => 5
+// console.log(Math.floor(4.7)); // below value => 4
+// console.log(Math.max(10,2,4,15,20)); //20
+// console.log(Math.min(10,2,4,15,20)); //2
+
+
+/*Random function generates a random integer bw 0 & 1
+Multiplying it with 100 gives a two digit number before decimal */
+const random = Math.random()*100;
+// console.log(random.toPrecision(2));
+
+// console.log((Math.random()*10) + 1); // gives values bw 1-10
+// console.log(Math.floor(Math.random()*10) + 1); // floor function rounds off to below value
+
+/*
+Trick to effectively use random function in an application
+*/
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// Random number bw min and max value 
+// + min will make the value lie in the given range
